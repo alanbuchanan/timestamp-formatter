9736cd9e05fbfa6dbee04460774bb2a51e805760
Rory Smith <rory.smith@korelogic.co.uk>
2017-12-16 15:06:27 +0000

    Write tests and app

--- src/timestamp-formatter.js
+++ src/timestamp-formatter.js
@@ -1,11 +1,81 @@
-function timestampFormatter(timestamp) {
-  var today = new Date(timestamp * 1000);
-  var prependZero = num => num < 10 ? `0${num}` : num
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1;
-  var yyyy = today.getFullYear();
 
-  return `${prependZero(dd)}/${prependZero(mm)}/${yyyy}`;
+var validFormats = [
+  'dd/mm/yy',
+  'dd/mm/yyyy',
+  'd/m/yy',
+  'd/m/yyyy',
+  'dd-mmm-yy',
+  'dd-mmm-yyyy',
+  'd-mmm-yy',
+  'd-mmm-yyyy',
+  'd-mmmm-yy',
+  'd-mmmm-yyyy',
+  'dd-mm-yy',
+  'yy/mm/dd',
+  'yyyy/mm/dd',
+  'mm/dd/yy',
+  'mm/dd/yyyy',
+  'mmm-dd-yy',
+  'mmm-dd-yyyy',
+  'yyyy-mm-dd',
+  'mmm-yy',
+  'yy',
+  'yyyy'
+];
+
+var months = [
+  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+function timestampFormatter(timestamp, format) {
+  if (!timestamp) {
+    return 'Oops! That timestamp is not valid. Please ensure you are passing in a valid UNIX timestamp as your `timestamp` argument.'
+  }
+  timestamp = timestamp.toString().trim();
+
+  if (!format) {
+    format = 'dd/mm/yyyy';
+  }
+
+  var formatMatchesValidFormats = validFormats.indexOf(format) !== -1;
+
+  if (!formatMatchesValidFormats) {
+    return 'Oops! That format is not valid. Please ensure your format is one of ' + validFormats.toString();
+  }
+
+  var formatIncludesDelimiter = ['-', '/'].some(delimiter => format.indexOf(delimiter) !== -1);
+
+  var prependZero = num => num < 10 ? `0${num}` : num
+
+  var date = new Date(timestamp * 1000);
+
+  var d = date.getDate();
+  var dd = prependZero(date.getDate());
+  var m = date.getMonth() + 1;
+  var mm = prependZero(date.getMonth() + 1);
+  var mmmm = months[date.getMonth()];
+  var mmm = mmmm.substr(0, 3);
+  var yy = date.getFullYear().toString().substr(-2);
+  var yyyy = date.getFullYear();
+
+  const dict = {
+    d,
+    dd,
+    m,
+    mm,
+    mmm,
+    mmmm,
+    yy,
+    yyyy
+  };
+
+  var newDates = [];
+  var delimiter = format.indexOf('/') !== -1 ? '/' : '-';
+  var dateSplit = format.split(delimiter);
+
+  dateSplit.forEach(item => newDates.push(dict[item]));
+
+  return newDates.join(delimiter);
 }
 
 module.exports = timestampFormatter;
--- test/timestamp-formatter.test.js
+++ test/timestamp-formatter.test.js
@@ -1,35 +1,51 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var timestampFormatter = require('../src/timestamp-formatter');
 describe('timestamp-formatter', function() {
   it('should return dd/mm/yyyy format by default', function() {
     assert.equal(timestampFormatter('1513378246'), '15/12/2017');
     assert.equal(timestampFormatter('0'), '01/01/1970');
     assert.equal(timestampFormatter('253402300800'), '01/01/10000');
-    assert.equal(timestampFormatter('-788918400'), '01/01/1945');
-    assert.equal(timestampFormatter('1525132800'), '01/05/2018');
   });
-  it.skip('should return an error message if either the timestamp or the format is invalid', function() {
+  it('should work if you pass in spaces on the start or end of either the timestamp or the format', function() {
+    assert.equal(timestampFormatter('0'), '01/01/1970');
+  });
+  it('should be able to take a number as a timestamp', function() {
+    assert.equal(timestampFormatter(1513378246), '15/12/2017');
+    assert.equal(timestampFormatter(-788918400), '01/01/1945');
+    assert.equal(timestampFormatter(123456789), '29/11/1973');
+  });
+  it('should return an error message if the timestamp is invalid', function() {
+    assert.include(timestampFormatter(null), 'not valid');
+    assert.include(timestampFormatter(undefined), 'not valid');
+  });
+  it('should return an error message if the format is invalid', function() {
+    assert.include(timestampFormatter(0, undefined), 'not valid');
+  });
+  it('should work for forward-slash-delimited formats', function() {
+    assert.equal(timestampFormatter(123456789, 'dd/mm/yy'), '29/11/73');
+    assert.equal(timestampFormatter('253402300800', 'dd/mm/yy'), '01/01/00');
+  });
+  it('should work for hyphen-delimited formats', function() {
+    assert.equal(timestampFormatter(123456789, 'dd-mm-yy'), '29-11-73');
+    assert.equal(timestampFormatter('253402300800', 'dd-mm-yy'), '01-01-00');
+  });
 
+  it('should work when the format is in different orders', function() {
+    assert.equal(timestampFormatter('0', 'yy/mm/dd'), '70/01/01');
+    assert.equal(timestampFormatter('0', 'yyyy/mm/dd'), '1970/01/01');
+    assert.equal(timestampFormatter(-788918400, 'dd-mmm-yy'), '01-Jan-45');
+    assert.equal(timestampFormatter(-788918400, 'dd-mmm-yyyy'), '01-Jan-1945');
+    assert.equal(timestampFormatter('390294397', 'dd-mmm-yyyy'), '15-May-1982');
+    assert.equal(timestampFormatter('390294397', 'd-mmmm-yyyy'), '15-May-1982');
   });
-  it.skip('should work for forward-slash-delimited formats', function() {
-
+  it('should work for d and m with no leading 0', function() {
+    assert.equal(timestampFormatter('253402300800', 'd/m/yy'), '1/1/00');
+    assert.equal(timestampFormatter('0', 'd/m/yy'), '1/1/70');
+    assert.equal(timestampFormatter(1513417634, 'd-mmmm-yy'), '16-December-17');
+    assert.equal(timestampFormatter(1513417634, 'd/m/yy'), '16/12/17');
   });
-  it.skip('should work for hyphen-delimited formats', function() {
-    
-  });
-  it.skip('should work when there is no delimiter', function() {
-
-  });
-  it.skip('should return the day, month, or year if only one of those is passed', function() {
-
-  });
-  it.skip('should work for a weekday format', function() {
-
-  });
-  it.skip('should be able to return the time', function() {
-
-  });
-  it.skip('should be able to return the time as part of the date', function() {
-
+  it('should return the day, month, or year if only one of those is passed', function() {
+    assert.equal(timestampFormatter(1513417634, 'yy'), '17');
+    assert.equal(timestampFormatter(1513417634, 'yyyy'), '2017');
   });
 });
